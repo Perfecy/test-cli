33bceb0a1966ea0be43a7fdc86aa25b841e45615
Kirill <40721919+Perfecy@users.noreply.github.com>
2021-04-25 13:15:10 +0300

    comment useless

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const server = require('./server');
 server.serverWeb();
 
 commander
-    .version('1.0.0')
+    .version('0.0.1')
     .description('Configuration files creator.')
 
 commander
@@ -134,80 +134,80 @@ commander
     })
 
 
-commander
-    .command('create <name>')
-    .option('--extension <value>', 'File extension')
-    .alias('c')
-    .description('Create new configuration file.')
-    .action((name, cmd) => {
-        if (
-            cmd.extension &&
-            !['json', 'txt', 'cfg'].includes(cmd.extension)
-        ) {
-            console.log(chalk.red('\nExtension is not allowed.'))
-        } else {
-            prompt([
-                {
-                    type: 'input',
-                    name: 'charset',
-                    message: 'Charset: ',
-                },
-                {
-                    type: 'input',
-                    name: 'max_ram_usage',
-                    message: 'Max RAM usage, Mb: ',
-                },
-                {
-                    type: 'input',
-                    name: 'max_cpu_usage',
-                    message: 'Max CPU usage, %: ',
-                },
-                {
-                    type: 'input',
-                    name: 'check_updates_interval',
-                    message: 'Updates interval, ms: ',
-                },
-                {
-                    type: 'input',
-                    name: 'processes_count',
-                    message: 'Processes count: ',
-                },
-            ]).then((options) => {
-                if (cmd.extension && cmd.extension === 'json') {
-                    fs.writeFileSync(
-                        `files/${name}.${cmd.extension}`,
-                        JSON.stringify(options)
-                    )
-                } else {
-                    let data = ''
-                    for (let item in options)
-                        data += `${item}=${options[item]} \n`
+// commander
+//     .command('create <name>')
+//     .option('--extension <value>', 'File extension')
+//     .alias('c')
+//     .description('Create new configuration file.')
+//     .action((name, cmd) => {
+//         if (
+//             cmd.extension &&
+//             !['json', 'txt', 'cfg'].includes(cmd.extension)
+//         ) {
+//             console.log(chalk.red('\nExtension is not allowed.'))
+//         } else {
+//             prompt([
+//                 {
+//                     type: 'input',
+//                     name: 'charset',
+//                     message: 'Charset: ',
+//                 },
+//                 {
+//                     type: 'input',
+//                     name: 'max_ram_usage',
+//                     message: 'Max RAM usage, Mb: ',
+//                 },
+//                 {
+//                     type: 'input',
+//                     name: 'max_cpu_usage',
+//                     message: 'Max CPU usage, %: ',
+//                 },
+//                 {
+//                     type: 'input',
+//                     name: 'check_updates_interval',
+//                     message: 'Updates interval, ms: ',
+//                 },
+//                 {
+//                     type: 'input',
+//                     name: 'processes_count',
+//                     message: 'Processes count: ',
+//                 },
+//             ]).then((options) => {
+//                 if (cmd.extension && cmd.extension === 'json') {
+//                     fs.writeFileSync(
+//                         `files/${name}.${cmd.extension}`,
+//                         JSON.stringify(options)
+//                     )
+//                 } else {
+//                     let data = ''
+//                     for (let item in options)
+//                         data += `${item}=${options[item]} \n`
 
-                    fs.writeFileSync(`files/${name}.cfg`, data)
-                }
-                console.log(
-                    chalk.green(
-                        `\nFile "${name}.${cmd.extension || 'cfg'
-                        }" created.`
-                    )
-                )
-            })
-        }
-    })
+//                     fs.writeFileSync(`files/${name}.cfg`, data)
+//                 }
+//                 console.log(
+//                     chalk.green(
+//                         `\nFile "${name}.${cmd.extension || 'cfg'
+//                         }" created.`
+//                     )
+//                 )
+//             })
+//         }
+//     })
 
-commander
-    .command('all')
-    .alias('a')
-    .description('Show all configuration files.')
-    .action(() => {
-        const files = fs.readdirSync('files')
+// commander
+//     .command('all')
+//     .alias('a')
+//     .description('Show all configuration files.')
+//     .action(() => {
+//         const files = fs.readdirSync('files')
 
-        let data = ''
-        for (let file of files) data += `${file} \n`
+//         let data = ''
+//         for (let file of files) data += `${file} \n`
 
-        console.log(
-            chalk.grey(`\nConfiguration files: \n\n${data}`)
-        )
-    })
+//         console.log(
+//             chalk.grey(`\nConfiguration files: \n\n${data}`)
+//         )
+//     })
 
 commander.parse(process.argv)
